a96d02bbbc6e539f4417d9a56b3c302f8c639de0
TJWrds <itswrds@gmail.com>
2022-06-20 15:17:23 -0500

    fuck off

---
+++ Variables/variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "string"
+	fmt.Println(a)
+	a = "new string value"
+	fmt.Println(a)
+	a = "1"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var ef uint
+	ef = 100
+	fmt.Println(ef)
+
+	f := "apple"
+	fmt.Println(f)
+
+}
---
+++ values/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go"+"lang", "go", "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7/3 =", 7/3.14)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
